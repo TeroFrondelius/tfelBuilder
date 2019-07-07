5be7be37af5e657e3001c80639d7793056536334
Tero Frondelius <tero.frondelius@gmail.com>
2019-07-07 17:52:41 +0300

    Add autogenerated files to build tfelBuilder

---
+++ README.md
@@ -0,0 +1,9 @@
+# tfelBuilder
+
+[![Build Status](https://travis-ci.org/TeroFrondelius/tfelBuilder.svg?branch=master)](https://travis-ci.org/TeroFrondelius/tfelBuilder)
+
+This repository builds binary artifacts for the tfelBuilder project. Binary artifacts are automatically uploaded to
+[this repository's GitHub releases page](https://github.com/TeroFrondelius/tfelBuilder/releases) whenever a tag is created
+on this repository.
+
+This repository was created using [BinaryBuilder.jl](https://github.com/JuliaPackaging/BinaryBuilder.jl)
---
+++ build_tarballs.jl
@@ -0,0 +1,69 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder
+
+name = "tfelBuilder"
+version = v"0.1.0"
+
+# Collection of sources required to build tfelBuilder
+sources = [
+    "https://github.com/thelfer/tfel.git" =>
+    "c31d590f55508e4ab5fb3e22e898a2cb5af2a97a",
+
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir
+cd tfel/
+apk add gnuplot
+cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=/opt/$target/$target.toolchain
+make
+make install
+
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = [
+    Linux(:x86_64, libc=:glibc)
+]
+
+# The products that we will ensure are always built
+products(prefix) = [
+    ExecutableProduct(prefix, "mfront", :mfront),
+    LibraryProduct(prefix, "libMTestFileGenerator", :libMTestFileGenerator),
+    ExecutableProduct(prefix, "tfel-config", Symbol("tfel-config")),
+    LibraryProduct(prefix, "libMFrontLogStream", :libMFrontLogStream),
+    LibraryProduct(prefix, "libTFELTests", :libTFELTests),
+    ExecutableProduct(prefix, "tfel-doc", Symbol("tfel-doc")),
+    ExecutableProduct(prefix, "mfront-doc", Symbol("mfront-doc")),
+    LibraryProduct(prefix, "libTFELMathKriging", :libTFELMathKriging),
+    LibraryProduct(prefix, "libTFELGlossary", :libTFELGlossary),
+    LibraryProduct(prefix, "libTFELConfig", :libTFELConfig),
+    LibraryProduct(prefix, "libTFELMath", :libTFELMath),
+    LibraryProduct(prefix, "libTFELMTest", :libTFELMTest),
+    LibraryProduct(prefix, "libTFELMathParser", :libTFELMathParser),
+    LibraryProduct(prefix, "libTFELUtilities", :libTFELUtilities),
+    LibraryProduct(prefix, "libMFrontProfiling", :libMFrontProfiling),
+    LibraryProduct(prefix, "libTFELMathCubicSpline", :libTFELMathCubicSpline),
+    LibraryProduct(prefix, "libTFELMaterial", :libTFELMaterial),
+    LibraryProduct(prefix, "libTFELNUMODIS", :libTFELNUMODIS),
+    ExecutableProduct(prefix, "tfel-check", Symbol("tfel-check")),
+    LibraryProduct(prefix, "libTFELSystem", :libTFELSystem),
+    ExecutableProduct(prefix, "mfront-query", Symbol("mfront-query")),
+    ExecutableProduct(prefix, "mtest", :mtest),
+    LibraryProduct(prefix, "libTFELPhysicalConstants", :libTFELPhysicalConstants),
+    LibraryProduct(prefix, "libTFELException", :libTFELException),
+    LibraryProduct(prefix, "libTFELMFront", :libTFELMFront),
+    ExecutableProduct(prefix, "mfm", :mfm)
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+    
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
+
