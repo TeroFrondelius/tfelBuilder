1d0b90dc16533feb126692b966312d02ca95150c
Tero Frondelius <TeroFrondelius@users.noreply.github.com>
2019-07-07 22:35:48 +0300

    Update build_tarballs.jl

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -16,7 +16,7 @@ sources = [
 script = raw"""
 cd $WORKSPACE/srcdir/tfel/
 apk add gnuplot
-cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=/opt/$target/$target.toolchain
+cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=/opt/$target/$target.toolchain -Denable-portable-build=ON
 make
 make install
 
@@ -28,11 +28,11 @@ platforms = [
     Linux(:x86_64, libc=:glibc),
     Windows(:x86_64),
     Linux(:aarch64, libc=:glibc),
-    Linux(:i686, libc=:glibc),
-    Linux(:armv7l, libc=:glibc, call_abi=:eabihf),
-    Linux(:powerpc64le, libc=:glibc),
-    Linux(:i686, libc=:musl),
-    Linux(:x86_64, libc=:musl)
+    Linux(:i686, libc=:glibc)
+    # Linux(:armv7l, libc=:glibc, call_abi=:eabihf),
+    # Linux(:powerpc64le, libc=:glibc),
+    # Linux(:i686, libc=:musl),
+    # Linux(:x86_64, libc=:musl)
 ]
 
 # The products that we will ensure are always built
