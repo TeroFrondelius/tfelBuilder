2c84e1e50a174d5f0a1e6e152f79c4878f2eb1fe
Tero Frondelius <tero.frondelius@gmail.com>
2019-07-10 12:38:10 +0300

    Making travis parallel

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -14,9 +14,14 @@ sources = [
 
 # Bash recipe for building across all platforms
 script = raw"""
+export COMMON_FLAGS=\
+"-DCMAKE_INSTALL_PREFIX=$prefix "\
+"-DCMAKE_TOOLCHAIN_FILE=/opt/$target/$target.toolchain "\
+"-Denable-portable-build=ON "
+
 cd $WORKSPACE/srcdir/tfel/
 apk add gnuplot
-cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=/opt/$target/$target.toolchain -Denable-portable-build=ON
+cmake $COMMON_FLAGS
 make
 make install
 
@@ -24,17 +29,7 @@ make install
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = [
-    Linux(:x86_64, libc=:glibc),
-    Windows(:x86_64),
-    Linux(:aarch64, libc=:glibc),
-    Linux(:i686, libc=:glibc)
-    # MacOS()
-    # Linux(:armv7l, libc=:glibc, call_abi=:eabihf),
-    # Linux(:powerpc64le, libc=:glibc),
-    # Linux(:i686, libc=:musl),
-    # Linux(:x86_64, libc=:musl)
-]
+platforms = supported_platforms()
 
 # The products that we will ensure are always built
 products(prefix) = [
